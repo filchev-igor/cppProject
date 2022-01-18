1869bafb55d041d33d962b88ac7c9a5b873034fb
Igor Filchev <filchev.igor@gmail.com>
2022-01-18 23:48:29 +0200

    Buttons added. Code for enabling state of the buttons and their color was written

---
+++ windowsForms/StudentsApp/StartForm.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace StudentsApp
+{
+    partial class StartForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Tomato;
+            this.button1.Enabled = false;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(209, 62);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(388, 69);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Create file";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Tomato;
+            this.button3.Enabled = false;
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(209, 159);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(388, 69);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Write auto generated data into file";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Tomato;
+            this.button2.Enabled = false;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(209, 257);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(388, 69);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Sort file data and export";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(209, 355);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.PlaceholderText = "Type file name to activate buttons";
+            this.textBox1.Size = new System.Drawing.Size(388, 27);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // StartForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "StartForm";
+            this.Text = "Type name of file";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
---
+++ windowsForms/StudentsApp/StartForm.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentsApp
+{
+    public partial class StartForm : Form
+    {
+        private string _fileName;
+
+        public StartForm()
+        {
+            InitializeComponent();
+        }
+
+        private void disableButtons(Button activeButton)
+        {
+            if (activeButton != button1)
+            {
+                button1.Enabled = false;
+                button1.BackColor = Color.Tomato;
+            }
+
+            if (activeButton != button2)
+            {
+                button2.Enabled = false;
+                button2.BackColor = Color.Tomato;
+            }
+
+            if (activeButton != button3)
+            {
+                button3.Enabled = false;
+                button3.BackColor = Color.Tomato;
+            }
+        }
+
+        private void enableButtons()
+        {
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = true;
+
+            button1.BackColor = Color.SteelBlue;
+            button2.BackColor = Color.SteelBlue;
+            button3.BackColor = Color.SteelBlue;
+        }
+
+        private void createFile(string fileName)
+        {
+
+            std::ofstream fileOut;
+
+            fileOut.open(fileName, ios_base::in);
+
+            if (fileOut)
+                return;
+
+            fileOut.open(fileName, ios_base::app);
+
+            fileOut << "Pavarde Vardas ND1 ND2 ND3 ND4 ND5 ND6 ND7 ND8 ND9 ND10 Egzaminas" << endl;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string fileName = _fileName;
+
+            disableButtons(button1);
+
+            this._fileName = "";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string fileName = textBox1.Text.Trim();
+
+            if (fileName.Length < 1)
+            {
+                disableButtons();
+
+                return;
+            }
+
+            enableButtons();
+
+            this._fileName = fileName;
+        }
+    }
+}
